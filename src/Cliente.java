24d4d72af86cb215ad5a8bbec5cd6b4cb5177df7
Barca <marcomatiaspg@gmail.com>
2017-05-26 22:37:08 +0100

    construtor vazio

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,9 +1,20 @@
 
 public class Cliente extends Utilizador{
+    
     // construtor
-    public Cliente(String fname, String lname,String email, 
-        String username, String password, String morada, String dataNasc){
-        super(fname, lname, email, username, password, morada, dataNasc);
+    public Cliente(int type, double gpsX, double gpsY, String fname, 
+        String lname,String email, String username, String password, 
+        String morada, String dataNasc){
+        super(type, gpsX, gpsY, fname, lname, email, username, password, 
+            morada, dataNasc);
+        this.setType(1);
     }
     
+    public Cliente(){
+    
+    }
+    
+ //   public Cliente(){
+        
+        
 }
