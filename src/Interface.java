51aabeee7e2dce2b3ff1b3dcf41cdc29c04a3b0f
Barca <marcomatiaspg@gmail.com>
2017-05-26 19:48:19 +0100

    add interface

---
+++ src/Interface.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Interface here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Interface
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Interface
+     */
+    public Interface()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+   // login
+   public int login(String username, String pwd){
+       
+       getLogIn(username, pwd);
+    }
+   // açoe
+       
+}
