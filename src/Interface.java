c9ad6225fed6ca23883e97746a04b751ba701ff5
Barca <marcomatiaspg@gmail.com>
2017-05-26 22:38:06 +0100

    minor fix

--- src/Interface.java
+++ src/Interface.java
@@ -1,28 +1,48 @@
-
+import java.util.HashMap;
 /**
  * Write a description of class Interface here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Interface
-{
-    // instance variables - replace the example below with your own
-    private int x;
+public class Interface{
+    private CatalogoUser catalogo;
+    private int type = 0;
 
-    /**
-     * Constructor for objects of class Interface
-     */
-    public Interface()
-    {
-        // initialise instance variables
-        x = 0;
-    }
+   public Interface(){
+     
+   }
    // login
-   public int login(String username, String pwd){
+   
+   // return 2 password errada return 1 erro do utilizador
+   public int login(CatalogoUser data, String username, String pwd){
+       Utilizador cliente = new Cliente();
+       Utilizador motorista = new Motorista();
        
-       getLogIn(username, pwd);
+       if (data.checkUser(username)){
+           
+           if (data.isCliente(data.getUser(username))){
+               cliente = data.getUser(username);
+               type = 1;
+           } else{
+               motorista = data.getUser(username);
+               type = 2;
+           }
+           
+           if (type == 1 && pwd != cliente.getPassword()) return 2;
+           if (type == 2 && pwd != motorista.getPassword()) return 2;
+       }
+       return 0;
     }
+
    // açoe
-       
+   /**
+   private static startAsCliente(Utilizador a){
+       newJurney(a.
+    }
+   
+   private static startAsMototrista(Utilizador a){
+    
+   }
+   */
 }
