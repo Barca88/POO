3624b04f1c13e757a4229e47ccfe579da283d7c5
Barca <marcomatiaspg@gmail.com>
2017-05-28 23:24:16 +0100

    TreeMap Streamable

--- src/CatalogoTaxi.java
+++ src/CatalogoTaxi.java
@@ -1,33 +1,20 @@
-
 /**
- * Write a description of class CatalogoTaxi here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class CatalogoTaxi
-{
-    // instance variables - replace the example below with your own
-    private int x;
+import java.util.HashMap;
 
-    /**
-     * Constructor for objects of class CatalogoTaxi
-     */
-    public CatalogoTaxi()
-    {
-        // initialise instance variables
-        x = 0;
-    }
+public class CatalogoTaxi {
+    private HashMap<Long,Taxi> taxis;
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public CatalogoTaxi(){
+        taxis = new HashMap<>();        
     }
+ 
+    public boolean checkTaxi(long id){
+        return taxis.containsKey(id);
+    }
+    
+    public Utilizador getTaxi(long id){
+        return users.get(taxi);
+    }
+    
 }
+*/
