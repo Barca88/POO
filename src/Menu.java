9130411d313d82bbe9d1e68bf4303347deec1a2f
Barca <marcomatiaspg@gmail.com>
2017-05-31 18:09:28 +0100

    minor changes

--- src/Menu.java
+++ src/Menu.java
@@ -2,56 +2,68 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.InputMismatchException;
-import java.io.Serializable;
 
-public class Menu implements Serializable {
-	
-	private List<String> opcoes;
-	private int op;
+public class Menu {
+    // variáveis de instância
+    private List<String> opcoes;
+    private int op;
 
-	public Menu (String[] opcoes){
-		this.opcoes = new ArrayList<String>();
-		for(String op: opcoes)
-			this.opcoes.add(op);
-		this.op = 0;
-	}
+    /**
+     * Constructor for objects of class Menu
+     */
+    public Menu(String[] opcoes) {
+        this.opcoes = new ArrayList<String>();
+        for (String op : opcoes)
+            this.opcoes.add(op);
+        this.op = 1;
+    }
 
-	public void executa(){
-		do{
-			showMenu();
-			this.op = lerOpcao();
-		} while (this.op == -1);
-	}
+    /**
+     * Método para apresentar o menu e ler uma opção.
+     *
+     */
+    public void executa(String nome) {
+        do {
+            showMenu(nome);
+            this.op = lerOpcao();
+        } while (this.op == -1);
+    }
 
-	private void showMenu(){
-		System.out.println("\n *** Menu *** ");
-		for (int i=0; i < this.opcoes.size(); i++){
-			System.out.print(i+1);
-			System.out.print(" - ");
-			System.out.println(this.opcoes.get(i));
-		}
-		System.out.println("0 - Sair");
-	}
+    /** Apresentar o menu */
+    private void showMenu(String nome) {
+        System.out.println("\n    |========== IMOBILIARIA ===========| ");
+        if(nome != null)System.out.println("       Sessão iniciada - "+"'"+nome+"'");
+        for (int i=0; i<this.opcoes.size(); i++) {
+            System.out.print(i+1);
+            System.out.print(" - ");
+            System.out.println(this.opcoes.get(i));
+        }
+        System.out.println("0 - Sair");
+    }
 
-	private int lerOpcao(){
-		int op;
-		Scanner is = new Scanner(System.in);
+    /** Ler uma opção válida */
+    private int lerOpcao() {
+        int op;
+        Scanner is = new Scanner(System.in);
 
-		System.out.println("Opção: ");
-		try{
-			op = is.nextInt();
-		}
-		catch(InputMismatchException e){
-			op = -1;
-		}
-		if (op<0 || op>this.opcoes.size()){
-			System.out.println("Opção Inválida!");
-			op = -1;
-		}
-		return op;
-	}
+        System.out.print("Opção: ");
+        try {
+            op = is.nextInt();
+        }
+        catch (InputMismatchException e) { // Não foi inscrito um int
+            op = -1;
+        }
+        if (op<0 || op>this.opcoes.size()) {
+            System.out.println("Opção Inválida!!!");
+            op = -1;
+        }
+        return op;
+    }
 
-	public int getOpcao(){
-		return this.op;
-	}
+    /**
+     * Método para obter a última opção lida
+     */
+    public int getOpcao() {
+        return this.op;
+    }
 }
