e0aabed4e55e791ce50228833a3e834e1628b1f5
Barca <marcomatiaspg@gmail.com>
2017-05-31 05:10:54 +0100

    Viagem

--- src/Viagem.java
+++ src/Viagem.java
@@ -1,68 +1,108 @@
 public class Viagem{
 
     private double preco;
-    //duracao
-    //Inicio da viagem
-    //fim de viagem
+    private Localizacao pICliente;
+    private Localizacao pITaxi;
+    private Localizacao pDestino;
     private Cliente cliente;
-    private Taxi carro;
+    private Taxi taxi;
+    private Motorista motorista;
     //data
 
     public Viagem(){
-    	this.preco=0.0;
-    	//
-    	//
-    	//
-    	//
-    	//
+    	this.preco = 0.0;
+        this.pICliente = new Localizacao();
+        this.pITaxi = new Localizacao();
+        this.pDestino = new Localizacao();
+        this.cliente = new Cliente();
+        this.taxi = new taxi();
+        this.motorista = new Motorista();
     }
 
     public Viagem(double preco){
-    	this.preco=preco;
-    	//
-    	//
-    	//
+    	this.preco = preco;
+        //
     }
 
     public Viagem(Viagem v){
-    	this.preco=v.getPreco();
-    	this.motorista=v.getMotorista();
-    	this.cliente=v.getCliente();
-    	//
-    	//
+    	this.preco = v.getPreco();
+    	this.lICliente = v.getLICliente();
+        this.lITaxi = v.getLITaxi();
+        this.lDestino = v.getLDestino();
+        this.cliente = v.getCliente();
+        this.taxi = v.getTaxi();
+        this.motorista = v.getMotorista();
     }
-
-    //Metodos
-
+    //Getters
     public double getPreco(){
     	return preco;
     }
-
+    public Localizacao getLICliente(){
+    	return lICliente;
+    }
+    public Localizacao getLITaxi(){
+    	return lITaxi;
+    }
+    public Localizacao getLDestino(){
+    	return lDestino;
+    }
+    public Cliente getCliente(){
+        return cliente;
+    }
+    public Taxi getTaxi(){
+        return taxi;
+    }
     public Motorista getMotorista(){
     	return motorista;
     }
 
-    public Cliente getCliente(){
-    	return cliente;
-    }
-
+    //Setters
     public void setPreco(double preco){
-    	this.preco=preco;
+        this.preco = preco;
     }
-
-    public void setMotorista(Motorista motorista){
-    	this.motorista=motorista;
+    public void setLICliente(Localizacao lICLiente){
+        this.lICliente = lICliente;
     }
-
+    public void setLITaxi(Localizacao lITaxi){
+        this.lITaxi = lITaxi;
+    }
+    public void setLDestino(Localizacao lDestino){
+        this.lDestino = lDestino;
+    }
+    public void setTaxi(Taxi taxi){
+        this.taxi = taxi;
+    }
     public void setCliente(Cliente cliente){
-    	this.cliente=cliente;
+    	this.cliente = cliente;
+    }
+    public void setMotorista(Motorista motorista){
+    	this.motorista = motorista;
+    }
+
+    //Metodos
+    public double precoViagem(Taxi taxi, Cliente cliente,
+                                            Localizacao destino){
+        double custo;
+        custo = Distancia(taxi.getLocal(),cliente.getLocal());
+        custo = custo + Distancia(cliente.getLocal(),destino);
+        custo = custo * taxi.getPreco();
+        return custo;
+    }
+    public double tempoViagem(Taxi taxi, Cliente cliente,
+                                            Localizacao destino){
+        double tempo = TempoDesloca
+            (taxi.getVelMedia(),taxi.getLocal,cliente.getLocal());
+        tempo = tempo + TempoDesloca(taxi.getVelMedia(),
+                                    cliente.getLocal(),destino);
+        return tempo;
     }
 
 
 
 
 
-    
+
+
 
 
 }
