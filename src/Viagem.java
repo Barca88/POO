572899008f56bb0148ed839a9d1a76c213619126
Barca <marcomatiaspg@gmail.com>
2017-05-31 21:02:01 +0100

    equals

--- src/Viagem.java
+++ src/Viagem.java
@@ -1,57 +1,71 @@
 public class Viagem{
 
     private double preco;
-    private Localizacao pICliente;
-    private Localizacao pITaxi;
-    private Localizacao pDestino;
+    private Localizacao liCliente;
+    private Localizacao liTaxi;
+    private Localizacao liDestino;
     private Cliente cliente;
     private Taxi taxi;
     private Motorista motorista;
     //data
 
     public Viagem(){
-    	this.preco = 0.0;
-        this.pICliente = new Localizacao();
-        this.pITaxi = new Localizacao();
-        this.pDestino = new Localizacao();
+        this.preco = 0.0;
+        this.liCliente = new Localizacao();
+        this.liTaxi = new Localizacao();
+        this.liDestino = new Localizacao();
         this.cliente = new Cliente();
         this.taxi = new taxi();
         this.motorista = new Motorista();
     }
-
-    public Viagem(double preco){
-    	this.preco = preco;
-        //
+    
+    public Viagem(double preco, Localizacao pICliente, Localizacao pITaxi,
+                  Localizacao pDestino, Cliente cliente, 
+                  Taxi taxi, Motorista motorista){
+        this.preco = preco;
+        this.liCliente = liCliente;
+        this.liTaxi = liTaxi;
+        this.liDestino = liDestino;
+        this.cliente = cliente;
+        this.taxi = taxi;
+        this.motorista = motorista;
+        
     }
-
     public Viagem(Viagem v){
     	this.preco = v.getPreco();
-    	this.lICliente = v.getLICliente();
-        this.lITaxi = v.getLITaxi();
-        this.lDestino = v.getLDestino();
+    	this.liCliente = v.getLiCliente();
+        this.liTaxi = v.getLiTaxi();
+        this.liDestino = v.getLiDestino();
         this.cliente = v.getCliente();
         this.taxi = v.getTaxi();
         this.motorista = v.getMotorista();
     }
+    
     //Getters
     public double getPreco(){
     	return preco;
     }
-    public Localizacao getLICliente(){
-    	return lICliente;
+    
+    public Localizacao getLiCliente(){
+    	return liCliente;
     }
-    public Localizacao getLITaxi(){
-    	return lITaxi;
+    
+    public Localizacao getLiTaxi(){
+    	return liTaxi;
     }
-    public Localizacao getLDestino(){
-    	return lDestino;
+    
+    public Localizacao getLiDestino(){
+    	return liDestino;
     }
+    
     public Cliente getCliente(){
         return cliente;
     }
+    
     public Taxi getTaxi(){
         return taxi;
     }
+    
     public Motorista getMotorista(){
     	return motorista;
     }
@@ -60,11 +74,11 @@ public class Viagem{
     public void setPreco(double preco){
         this.preco = preco;
     }
-    public void setLICliente(Localizacao lICLiente){
-        this.lICliente = lICliente;
+    public void setLiCliente(Localizacao liCLiente){
+        this.liCliente = liCliente;
     }
-    public void setLITaxi(Localizacao lITaxi){
-        this.lITaxi = lITaxi;
+    public void setLITaxi(Localizacao liTaxi){
+        this.liTaxi = liTaxi;
     }
     public void setLDestino(Localizacao lDestino){
         this.lDestino = lDestino;
@@ -96,13 +110,36 @@ public class Viagem{
                                     cliente.getLocal(),destino);
         return tempo;
     }
-
-
-
-
-
-
-
-
-
+    public boolean equals(Object obj) {
+        if(obj == this) {
+            return true;
+        }
+        if(obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Viagem o = (Viagem) obj;
+        
+        return super.equals(o) && o.getPreco() == this.getPreco() && 
+        o.getPreco() == this.getPreco() && o.getLICliente() == this.getLICliente() &&
+        o.getLTaxi() == this.getLTaxi() && o.getLDestino() == this.getLDestino() && 
+        o.getCliente() == this.getCliente() && o.getTaxi() == this.getTaxi() &&
+        o.getMotorista() == this.getMotorista();
+    }
+    public Viagem clone(){
+        return new Viagem(this);
+    }
+    public String toString(){
+        StringBuilder str;
+        str = new StringBuilder();
+        
+        
+    private double preco;
+    private Localizacao pICliente;
+    private Localizacao pITaxi;
+    private Localizacao pDestino;
+    private Cliente cliente;
+    private Taxi taxi;
+    private Motorista motorista;
+        
+    }
 }
