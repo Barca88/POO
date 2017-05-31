8d8210ffc031f0055fac4c8e65d7f1196318c405
Barca <marcomatiaspg@gmail.com>
2017-05-31 20:59:42 +0100

    equals clone tostring

--- src/Localizacao.java
+++ src/Localizacao.java
@@ -10,6 +10,12 @@ public class Localizacao{
         this.x = 0.0;
         this.y = 0.0;
     }
+    
+    public Localizacao(Localizacao l){
+        this.x = l.getX();
+        this.y = l.getY();
+    }
+    
     public Localizacao(double x,double y){
         this.x = x;
         this.y = y;
@@ -33,14 +39,39 @@ public class Localizacao{
 
     //Métodos
     public static double Distancia(Localizacao a, Localizacao b){
-        double dist = Math.sqrt((b.x - a.x)^2 + (b.y - b.y)^2);
+        double dist = Math.sqrt(Math.pow((b.getX() - a.getX()),2) + Math.pow((b.getY() - b.getY()),2));
         return dist;
     }
+
     public static double TempoDesloca(double VelMedia,
                 Localizacao a, Localizacao b){
         double dist = Distancia(a,b);
         double tempo = dist * VelMedia;
         return tempo;
     }
-
+    
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+        str.append(super.toString());
+        str.append("Localizacao: \n");
+        str.append("X: ");
+        str.append(this.x+"\n");
+        str.append("Y: ");
+        str.append(this.y+"\n");
+        return str.toString();
+    }
+    
+    public boolean equals(Object obj) {
+        if(obj == this)
+            return true;
+        if(obj == null || obj.getClass() != this.getClass())
+            return false;
+        Localizacao l = (Localizacao) obj;
+        return super.equals(l) && l.getX() == this.getX() && l.getY() == this.getY();
+    }
+    
+    public Localizacao clone(){
+        return new Localizacao (this);
+    }
+    
 }
