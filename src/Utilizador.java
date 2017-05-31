5b7717ee5edc1a36fd54997c6e8ed4cc5a7cd609
Barca <marcomatiaspg@gmail.com>
2017-05-31 05:10:31 +0100

    User

--- src/Utilizador.java
+++ src/Utilizador.java
@@ -2,111 +2,86 @@ import java.io.Serializable;
 
 public abstract class Utilizador implements Serializable{
     // var. de instancia
-    private double gpsX;
-    private double gpsY;
+    private Localizacao gps;
     private String nome;
     private String email;
     private String password;
     private String morada;
     private String dataNasc;
-   
-    // Construtor
+
+    // Construtores
     public Utilizador(){
-        this.gpsX = 0.0;
-        this.gpsY = 0.0;
+        this.gps = new Localizacao();
         this.nome = null;
         this.email = null;
         this.password = null;
         this.morada = null;
         this.dataNasc = null;
     }
-    
-    public Utilizador (double gpsX, double gpsY, String nome, String email, String password, 
+    public Utilizador (Localizacao gps, String nome, String email, String password,
         String morada, String dataNasc){
-        this.gpsX = gpsX;
-        this.gpsY = gpsY;
+        this.gps = gps;
         this.nome=nome;
         this.email = email;
         this.password = password;
         this.morada = morada;
         this.dataNasc = dataNasc;
     }
-    
     public Utilizador (Utilizador u){
-        this.gpsX = u.getGpsX();
-        this.gpsY = u.getGpsY();
-        this.nome=u.getNome();
+        this.gps = u.getLocal();
+        this.nome = u.getNome();
         this.email = u.getEmail();
         this.password = u.getPassword();
         this.morada = u.getMorada();
         this.dataNasc = u.getData();
     }
-    
+
     //Getters
-    
-    public double getGpsX(){
-        return gpsX;
+    public Localizacao getLocal(){
+        return gps;
     }
-    
-    public double getGpsY(){
-        return gpsY;
-    }
-    
     public String getNome(){
         return nome;
     }
-    
     public String getEmail(){
         return email;
     }
-    
     public String getPassword(){
         return password;
     }
-
     public String getMorada(){
         return morada;
     }
-    
     public String getData(){
         return dataNasc;
     }
-    
+
     //Setters
-    public void setGpsX(double gpsX){
-        this.gpsX = gpsX;
+    public void setLocal(Localizacao gps){
+        this.gps = gps;
     }
-    
-    public void setGpsY(double gpsY){
-        this.gpsY = gpsY;
-    }
-    
     public void setNome(String nome){
         this.nome = nome;
     }
-    
     public void setEmail(String email){
         this.email = email;
     }
-
     public void setPassword(String password){
         this.password = password;
     }
-    
     public void setMorada(String morada){
         this.morada = morada;
     }
-    
     public void setData(String dataNasc){
         this.dataNasc = dataNasc;
     }
-    
+
+    //Metodos
     public abstract Utilizador clone();
-       
     public boolean equals(Object obj){
         if (obj == this)
             return true;
-        if (obj ==null || obj.getClass() != this.getClass())
+        if (obj == null || obj.getClass() != this.getClass())
             return false;
         Utilizador u = (Utilizador) obj;
         return u.getEmail().equals(email)
@@ -115,7 +90,6 @@ public abstract class Utilizador implements Serializable{
         && u.getMorada().equals(morada)
         && u.getData().equals(dataNasc);
     }
-
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Email: ").append(email).append("\n");
