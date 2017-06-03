9655b6b65a19b8439450754ae2727d25a1011865
João Vieira <joaovieira_96@hotmail.com>
2017-06-03 05:57:15 +0100

    fix comparador

--- src/ComparadorCustos.java
+++ src/ComparadorCustos.java
@@ -1,12 +1,12 @@
 import java.util.Comparator;
 import java.util.Map;
 
-public class ComparadorCustos implements Comparator<Map.Entry<String,Utilizador>>{
+public class ComparadorCustos implements Comparator<Utilizador>{
 
 
-	public int compare (Map.Entry<String,Utilizador> p1, Map.Entry<String,Utilizador> p2){
-		Cliente c1 = (Cliente) p1.getValue();
-		Cliente c2 = (Cliente) p2.getValue();
+	public int compare (Utilizador p1, Utilizador p2){
+		Cliente c1 = (Cliente) p1;
+		Cliente c2 = (Cliente) p2;
 		double p = (c1.totalPreco() - c2.totalPreco());
 		if (p>0) return -1;
 		if (p<0) return 1;
